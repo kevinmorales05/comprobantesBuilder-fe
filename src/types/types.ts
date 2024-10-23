5564e3ffed0b10caa5c64d3e2f447eddbdc866cf
u548742577 <kdmorale@pichincha.com>
2024-10-23 14:53:11 -0600

    omit eslit to publish the project

--- src/types/types.ts
+++ src/types/types.ts
@@ -25,58 +25,8 @@ export interface Comprobante {
   saldo?: string;
 }
 
-export interface ComprobanteInput {
 
-}
-
-{
-    "Id Transaccion": "10244516",
-    "Fecha Recepcion": "17/09/2024 17:42:20",
-    "Importe Destino": "600000",
-    "Divisa Destino": "MXP",
-    "Fecha Estado": "17/09/2024 18:17:23",
-    "Estado": "T520",
-    "Razon Estado": "Operacion liquidada por SPEI",
-    "ConfirmationNM": "CLE10000552",
-    "Tipo Cuenta": "CBE",
-    "Cuenta": "'012180001673505629",
-    "Agente": "BCM",
-    "Acronimo Banco": "BBVA MEXICO",
-    "ID Movimiento Venta": "",
-    "Importe Origen": "600000",
-    "Divisa Origen": "MXP",
-    "Tipo Cambio": "1",
-    "Codigo Estado Origen": "CDM",
-    "Nombre Ord": "CONSULTORES LEGALES ESPECIALIZADOS GUADARRAMA S DE RL DE CV",
-    "Segundo Nom Ord": "",
-    "Apellido Ord": "",
-    "Apellido Mat Ord": "",
-    "Direccion Ord": "Sin Calle",
-    "Ciudad Ord": "CDMX",
-    "Estado Ord": "CDM",
-    "Pais Ord": "MEX",
-    "Codigo Zip Ord": "00000",
-    "Telefono Ord": "'5544332211",
-    "Nombre Ben": "SEPSA SERVICIOS INTEGRALES SA DE CV",
-    "Segundo Nom Ben": "",
-    "Apellido Ben": "",
-    "Apellido Mat Ben": "",
-    "Direccion Ben": "PROLONGACION MADERO NO. 4305 COL. FIERRO",
-    "Ciudad Ben": "MONTERREY",
-    "Estado Ben": "NLE",
-    "Pais Ben": "MEX",
-    "Codigo Zip Ben": "64590",
-    "Telefono Ben": "'5544332211",
-    "Titular": "SEPSA SERVICIOS INTEGRALES SA DE CV",
-    "Idperson": "3435014",
-    "Tipo Cambio Banxico": "19.5887",
-    "Agencia": "CONSULTORES LEGALES ESPECIALIZADOS GUADA",
-    "Sub Estado T329": "-",
-    "ClaveRastreoSpei": "'90703CLE1000055220240917006",
-    "Concepto_de_pago": "COMPRA GUADARRAMA CDMX\r"
-}
-
-interface Transaccion {
+export interface Transaccion {
     IdTransaccion: string;
     FechaRecepcion: string;
     ImporteDestino: string;
